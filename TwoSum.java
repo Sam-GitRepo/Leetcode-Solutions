99f334facfe4d07b37c6588ef1595961e68c9a46
Sam <smarttsamy@gmail.com>
2023-05-24 09:04:09 +0530

    first commit

---
+++ TwoSum.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {                                                 
+        for(int i = 0 ; i < nums.length;i++){             //iterating in 1st time in array
+            for(int j = i+1; j < nums.length; j++){     //iterating after element of the 1st loop 
+                if(nums[i] + nums[j] == target){        // checking for the sum of target
+                    return new int [] {i,j};            // return new array index
+                }
+            }
+        }
+    }
+}
